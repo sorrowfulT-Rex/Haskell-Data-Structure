467df0bd110065344b1557659322c874b2811b0c
Yitang Chen <yc4120@ic.ac.uk>
2021-03-04 18:52:40 +0800

    Removing the hierarchy that ArrayBased is List

--- ArrayBased.hs
+++ ArrayBased.hs
@@ -4,12 +4,12 @@ import           Data.Foldable
 
 import           List
 
-class List a => ArrayBased a where
+class ArrayBased a where
   newWithSize  :: Foldable f => Int -> f e -> a e
   physicalSize :: a e -> Int
   resize       :: Int -> a e -> a e
 
-class MList a => MArrayBased a where
+class MArrayBased a where
   mNewWithSize  :: Foldable f => Int -> f e -> ST s (a s e)
   mPhysicalSize :: a s e -> ST s Int
   mResize       :: Int -> a s e -> ST s (a s e)
