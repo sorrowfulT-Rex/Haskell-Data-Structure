8e1eacf2504171e346ab37dca35f72904a217382
Yitang Chen <yc4120@ic.ac.uk>
2021-03-07 15:35:42 +0800

    Unboxed Strict ST Reference

---
+++ Unboxed/MURef.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Unboxed.MURef where
+
+import           Control.Monad.ST
+import           Data.Array.Base
+import           Data.Array.ST
+import           Data.Array.Unboxed
+
+type MURef s a = STUArray s Int a
+
+newMURef :: MArray (STUArray s) a (ST s) => a -> ST s (MURef s a)
+newMURef = newArray (0, 0)
+
+modifySTRef :: MArray (STUArray s) a (ST s) => MURef s a -> (a -> a) -> ST s ()
+modifySTRef ptr f = do
+  v <- readMURef ptr
+  writeMURef ptr $! f v
+
+readMURef :: MArray (STUArray s) a (ST s) => MURef s a -> ST s a
+readMURef = flip readArray 0 
+
+writeMURef :: MArray (STUArray s) a (ST s) => MURef s a -> a -> ST s ()
+writeMURef = flip writeArray 0
