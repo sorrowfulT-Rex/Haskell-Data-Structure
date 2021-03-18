ce31fbef54d05f1c85f43070fc5b02c992bb29fb
Yitang Chen <yc4120@ic.ac.uk>
2021-03-19 00:05:10 +0800

    Rename MHeapQ to MHeapPQ

--- MMZKDS/MHeapPQ.hs
+++ MMZKDS/MHeapPQ.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
-module MMZKDS.MHeapQ where
+module MMZKDS.MHeapPQ where
 
 import           Control.Monad (when)
 import           Control.Monad.ST (ST, runST)
@@ -22,14 +22,15 @@ import           MMZKDS.PriorityQueue (MPriorityQueue(..))
 import           MMZKDS.Unboxed.MURef (MURef, newMURef, readMURef, writeMURef)
 import           MMZKDS.Unsafe
   (unsafeCopyArray, unsafeLeftChild, unsafeRightChild)
+import           MMZKDS.Utilities (initialSize)
 
--- | 'MHeapQ' is a min-heap implementing the 'MPriorityQueue' class.
+-- | 'MHeapPQ' is a min-heap implementing the 'MPriorityQueue' class.
 -- The heap is implemented with an internal @STArray@.
 -- It is expected that the type of its elements is an instance of 'Ord'.
 -- It may adds an element to anywhere in the array, but it always pops the 
 -- "smallest" element.
 -- 
-data MHeapQ e s = MHeapQ {
+data MHeapPQ e s = MHeapPQ {
   mHeapS :: MURef s Int,
   mHeapA :: STRef s (STArray s Int e)
   }
@@ -39,7 +40,7 @@ data MHeapQ e s = MHeapQ {
 -- MPriorityQueue Instance
 --------------------------------------------------------------------------------
 
-instance Ord a => MPriorityQueue MHeapQ a ST s where
+instance Ord a => MPriorityQueue MHeapPQ a ST s where
   mAdd = undefined
   mPop = undefined
 
@@ -48,11 +49,11 @@ instance Ord a => MPriorityQueue MHeapQ a ST s where
 -- MDS & MDSCons Instances
 --------------------------------------------------------------------------------
 
-instance Ord a => MDS (MHeapQ a) ST s where
-  clear :: MHeapQ a s -> ST s ()
+instance Ord a => MDS (MHeapPQ a) ST s where
+  clear :: MHeapPQ a s -> ST s ()
   clear = flip writeMURef 0 . mHeapS
 
-  copy :: MHeapQ a s -> ST s (MHeapQ a s)
+  copy :: MHeapPQ a s -> ST s (MHeapPQ a s)
   copy mh = do
     lR    <- readMURef (mHeapS mh) >>= newMURef
     arrST <- readSTRef (mHeapA mh)
@@ -60,23 +61,23 @@ instance Ord a => MDS (MHeapQ a) ST s where
     resST <- newArray_ bd
     unsafeCopyArray arrST resST bd
     aR    <- newSTRef resST
-    return $ MHeapQ lR aR
+    return $ MHeapPQ lR aR
 
-  size :: MHeapQ a s -> ST s Int
+  size :: MHeapPQ a s -> ST s Int
   size = readMURef . mHeapS
 
-instance Ord a => MDSCons [a] (MHeapQ a) ST s where
-  finish :: MHeapQ a s -> ST s [a]
+instance Ord a => MDSCons [a] (MHeapPQ a) ST s where
+  finish :: MHeapPQ a s -> ST s [a]
   finish mh = do
     let freeze' = freeze :: STArray s Int a -> ST s (Array Int a)
-    readMURef (mHeapS mh)
-      >>= flip fmap (readSTRef (mHeapA mh) >>= freeze') . (. toList) . take
+    readMURef (mHeapS mh) >>= 
+      flip fmap (readSTRef (mHeapA mh) >>= freeze') . (. toList) . take
 
-  new :: [a] -> ST s (MHeapQ a s)
+  new :: [a] -> ST s (MHeapPQ a s)
   new list = do
     let l   = length list
-    let nla = newListArray :: (Int, Int) -> [a] -> ST s (STArray s Int a)
-    arrST <- nla (0, l - 1) list
+    arrST <- (newListArray :: (Int, Int) -> [a] -> ST s (STArray s Int a)) 
+      (0, initialSize l - 1) list
     lR    <- newMURef l
     aR    <- newSTRef arrST
     let glc = flip unsafeLeftChild l
@@ -88,32 +89,32 @@ instance Ord a => MDSCons [a] (MHeapQ a) ST s where
             let Just lc = mlc
             vi  <- readArray arrST i
             vlc <- readArray arrST lc
-            when (vi > vlc) 
-              $ wt' i vlc >> wt' lc vi >> fixHead lc (glc lc) (grc lc)
+            when (vi > vlc) $ 
+              wt' i vlc >> wt' lc vi >> fixHead lc (glc lc) (grc lc)
           | otherwise     = do
             let Just lc = mlc
             let Just rc = mrc
             vi  <- readArray arrST i
             vlc <- readArray arrST lc
             vrc <- readArray arrST rc
+            
             when (vi > vlc || vi > vrc) $ if vlc < vrc 
               then wt' i vlc >> wt' lc vi >> fixHead lc (glc lc) (grc lc)
               else wt' i vrc >> wt' rc vi >> fixHead rc (glc rc) (grc rc)
     let toMinHeap mi
           | isNothing mi = return Nothing
-          | otherwise    = do
-            toMinHeap mlc
-            toMinHeap mrc
-            fixHead i mlc mrc
-            return mi
+          | otherwise 
+            = toMinHeap mlc >> toMinHeap mrc >> fixHead i mlc mrc >> return mi
           where
             i   = fromJust mi
             mlc = unsafeLeftChild i l
             mrc = unsafeRightChild i l
     toMinHeap $ Just 0
-    return $ MHeapQ lR aR
+    return $ MHeapPQ lR aR
 
 foo = runST $ do
   let n = 15
-  mh <- new [n, (n - 1)..1 :: Int] :: ST s (MHeapQ Int s)
-  (finish :: MHeapQ Int s -> ST s [Int]) mh
+  mh <- new [n, (n - 1)..1 :: Int] :: ST s (MHeapPQ Int s)
+  mi <- copy mh
+  clear mh
+  (finish :: MHeapPQ Int s -> ST s [Int]) mi
